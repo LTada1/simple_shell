98f431cd217594b83046a9e1493834293dc84aff
Lukatada <111013434+LTada1@users.noreply.github.com>
2023-12-14 02:10:36 +0100

    Shell

--- main.h
+++ main.h
@@ -8,13 +8,16 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
-char _stringcopy(char *str1, char *str2);
+char _strcopy(char *str1, char *str2);
 char *_strncat(char *dest, char *src, int n);
 int _putchar(char c);
 int _strlen(char *str);
 void prompt_shell(void);
 unsigned int check_delim(char, const char *);
+tokenize_Str(char *lineptr_copy, char *delim, char **argv);
 char *_strtok(char *, const char *);
 
 #endif /* _MAIN_H_ */
