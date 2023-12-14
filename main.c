f4d395a26885710bcdfdc268f62cf59d3340bae6
Lukatada <111013434+LTada1@users.noreply.github.com>
2023-12-14 04:42:09 +0100

    Main

--- main.c
+++ main.c
@@ -1,28 +1,6 @@
 #include "main.h"
 
 /**
- * tokenize_str - where program begins
- * @delim: delimeter
- * @argv: argument vector
- * @lineptr_copy: str
- * return: 0 on succes and -1 on failure
- */
-
-void tokenize_str(char *lineptr_copy, const char *delim, char **argv) {
-char *token;
-int i;
-
-token = strtok(lineptr_copy, delim);
-for (i = 0; token != NULL; i++)
-{
-argv[i] = malloc(sizeof(char) * (strlen(token) + 1));
-_strncopy(argv[i], token);
-token = strtok(NULL, delim);
-}
-argv[i] = NULL;
-}
-
-/**
  * main - where program begins
  * @argc: argument number
  * @argv: argument vector
