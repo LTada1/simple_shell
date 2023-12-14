9bce6ee3774f62b5084eaf613f57d44b0be42db1
Lukatada <111013434+LTada1@users.noreply.github.com>
2023-12-14 02:08:31 +0100

    Main.c

--- main.c
+++ main.c
@@ -1,32 +1,73 @@
 #include "main.h"
 
 /**
+ * tokenize_str - where program begins
+ * @delim: delimeter
+ * @argv: argument vector
+ * @lineptr_copy: str
+ * return: 0 on succes and -1 on failure
+ */
+
+void tokenize_Str(char *lineptr_copy, char *delim, char **argv)
+{
+char *token;
+int i;
+
+token = strtok(lineptr_copy, delim);
+for (i = 0; token != NULL; i++) {
+argv[i] = malloc(sizeof(char) * (strlen(token) + 1));  // +1 for the null terminator
+_strcpy(argv[i], token);
+token = strtok(NULL, delim);
+}
+
+/**
  * main - where program begins
  * @argc: argument number
  * @argv: argument vector
  * return: 0 on succes and -1 on failure
  */
 
-int main(int argc, char *argv[] __attribute__((unused)))
-{
-size_t len = 0;
-size_t nread;
-char *str = NULL;
+#include "main.h"
 
+int main(int ac __attribute__((umused)), char **argv)
+{
+char *prompt = "(Eshell) $ ";
+char *lineptr = NULL, *lineptr_copy = NULL;
+size_t n = 0;
+ssize_t nchars_read;
+const char *delim = " \n";
+int num_tokens = 0;
+char *token;
+int i;
+  
 while (1)
 {
-prompt_shell();
-if (argc != 2)
+printf("%s", prompt);
+nchars_read = getline(&lineptr, &n, stdin);
+if (nchars_read == -1)
 {
-return EXIT_FAILURE;
+return (-1);
 }
-nread = getline(&str, &len, stdin);
-if (nread < 1 || feof(stdin))
+lineptr_copy = malloc(sizeof(char) * nchars_read);
+if (lineptr_copy == NULL)
 {
-printf("\nExiting shell.\n");
-break;
+perror("tsh: memory allocation error");
+return (-1);
 }
+_strcopy(lineptr_copy, lineptr);
+token = strtok(lineptr, delim);
+while (token != NULL)
+{
+num_tokens++;
+token = strtok(NULL, delim);
 }
-free(str);
-return 0;
+num_tokens++;
+argv = malloc(sizeof(char *) * num_tokens);
+token = tokenize_tring(lineptr_copy, delim, argv);
+argv[i] = NULL;
+execute_cmd(argv);
+}
+free(lineptr_copy);
+free(lineptr);
+return (0);
 }
