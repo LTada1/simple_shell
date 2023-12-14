2ea93a67a2c06478eb4db0eeec79a069c71c581b
Sanni <sandae20v@gmail.com>
2023-12-14 05:42:24 +0100

    getline aded

--- main.h
+++ main.h
@@ -14,12 +14,16 @@
 void execute_cmd(char **argv);
 char *get_loc(char *command);
 char _strncopy(char *str1, char *str2);
-char *_strncat(char *dest, char *src);
+char *_strncat(char *iest, char *src);
 int _putchar(char c);
 int _strlen(char *str);
 void prompt_shell(void);
 unsigned int check_delim(char, const char *);
 void tokenize_str(char *lineptr_copy, const char *delim, char **argv);
 char *_strtok(char *, const char *);
+char *_getline();
+char *enter(char *str);
+char *space(char *str);
+char *hashtag_handler(char *buff);
 
 #endif /* _MAIN_H_ */
