3a73c6d56751ce3c6e205e2826dc93539946847e
Lukatada <111013434+LTada1@users.noreply.github.com>
2023-12-15 23:29:52 +0100

    Update _strcat.c

--- _strcat.c
+++ _strcat.c
@@ -8,14 +8,16 @@
  *
  * Return: Always 0.
  */
-char *_strncat(char *dest, char *src)
-{
-	int len1, len2;
 
-	len2 = strlen(dest);
-	for (len1 = 0; *src != 0; len1++)
-	{
-		dest[len2++] = src[len1];
-	}
-	return (dest);
+char *_strncat(char *dest, const char *src, size_t n)
+{
+size_t len1, len2;
+
+len2 = _strlen(dest);
+for (len1 = 0; len1 < n && src[len1] != '\0'; len1++)
+{
+dest[len2++] = src[len1];
+}
+dest[len2] = '\0';
+return dest;
 }
