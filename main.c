94751911a566c015f2695938abf759621d640e44
Lukatada <111013434+LTada1@users.noreply.github.com>
2023-12-16 01:45:27 +0100

    Update main.c

--- main.c
+++ main.c
@@ -7,52 +7,40 @@
  * return: 0 on succes and -1 on failure
  */
 
-#include "main.h"
-
 int main(int argc, char **argv)
 {
-char *lineptr = NULL, *lineptr_copy = NULL;
+char *lineptr = NULL, *lineptr_copy = NULL, *token;
 size_t n = 0;
 ssize_t nchars_read;
 const char *delim = " \n";
-int num_tokens = 0;
-char *token;
-int i;
+int num_tokens, i;
 
 (void)argc;
-  
+
 while (1)
 {
 prompt_shell();
-nchars_read = getline(&lineptr, &n, stdin);
-if (nchars_read == -1) {
+if ((nchars_read = getline(&lineptr, &n, stdin)) == -1)
 return -1;
-}
-lineptr_copy = malloc(sizeof(char) * nchars_read);
-if (lineptr_copy == NULL)
+if (!(lineptr_copy = malloc(nchars_read)))
 {
 perror("tsh: memory allocation error");
 return -1;
 }
 _strncopy(lineptr_copy, lineptr);
-token = strtok(lineptr, delim);
-while (token != NULL) {
+num_tokens = 1;
+for (token = strtok(lineptr, delim); token; token = strtok(NULL, delim))
 num_tokens++;
-token = strtok(NULL, delim);
-}
-num_tokens++;
-argv = malloc(sizeof(char *) * num_tokens);
-if (argv == NULL)
+if (!(argv = malloc(sizeof(char *) * num_tokens)))
 {
 perror("Memory allocation error");
-return -1;
+return (-1);
 }
 tokenize_str(lineptr_copy, delim, argv);
 argv[num_tokens - 1] = NULL;
 execute_cmd(argv);
-for (i = 0; i < num_tokens - 1; i++) {
+for (i = 0; i < num_tokens - 1; i++)
 free(argv[i]);
-}
 free(argv);
 free(lineptr_copy);
 }
